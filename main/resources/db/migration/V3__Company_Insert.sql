4c604426aef8dee139bb9a08aa52eddd98c24c48
Crescent17 <maratos2001@mail.ru>
2022-03-11 15:23:18 +0300

    feat: modificate flyway scripts

--- main/resources/db/migration/V3__Company_Insert.sql
+++ main/resources/db/migration/V3__Company_Insert.sql
@@ -1,6 +1,6 @@
 insert into company(name, username, password)
-values ('Apple', 'apple@icloud.com', '123456');
+values ('Apple', 'apple@icloud.com', '$2a$12$L/x4bR.qxkyT6g66bwOtau/hFV2LfuboAIitJ4r.7iNJP0R8bA/lO');
 insert into company(name, username, password)
-values ('Amazon', 'amazon@gmail.com', '123456');
+values ('Amazon', 'amazon@gmail.com', '$2a$12$HPbFmAbpi93jfgnBQL//E.s0Wo7CDD9oaBmSdVjzTJK27cBnFVHBe');
 insert into company(name, username, password)
-values ('Nike', 'nike@gmail.com', '123456');
+values ('Nike', 'nike@gmail.com', '$2a$12$Jr4fixJKqrIuArmyieSZT.1UJBthIjUS.3XHCbWkpTbRpAIdEXkKu');
--- main/resources/db/migration/V6__Employee_Insert.sql
+++ main/resources/db/migration/V6__Employee_Insert.sql
@@ -1,3 +1,18 @@
 insert into employee(name, last_Name, username, password, company_name, company_id)
 values ('Alex', 'Jackson', 'alex@gmail.com',
-        '123456', 'Nike', 3)
+        '$2a$12$PNkqw7cxNAzILGl3qTFFpOEmLPXJkJ9eeg0dS5O1Nu9qDpLHdoG3.', 'Nike', 3);
+insert into employee(name, last_Name, username, password, company_name, company_id)
+values ('Mike', 'Hendersen', 'hendersen@gmail.com',
+        '$2a$12$urDgtq01BDzPfJXtiHoPne5byJYsM5ScWsBNpopyFIpleKNw/5dIS', 'Amazon', 2);
+insert into employee(name, last_Name, username, password, company_name, company_id)
+values ('John', 'White', 'white@gmail.com',
+        '$2a$12$mtG7rYKQLGUST9XkyMCdfecPRSwDQPyq1nec0zlKRxVYTUq/dePAy', 'Apple', 1);
+insert into employee(name, last_Name, username, password, company_name, company_id)
+values ('Jake', 'Brown', 'brown@gmail.com',
+        '$2a$12$.Wv5o5hocrOygonHqfjKSeVt2FZmiGo9WQD9YruNju.peemiORe/i', 'Nike', 3);
+insert into employee(name, last_Name, username, password, company_name, company_id)
+values ('Anna', 'Shaley', 'shaley@gmail.com',
+        '$2a$12$1./iZi/V44fjuVCXfzBJM.LS8Pgbegnpaxm4cCKLEu3eD4mjzBjHC', 'Amazon', 2);
+insert into employee(name, last_Name, username, password, company_name, company_id)
+values ('Kevin', 'Lesley', 'lesley@gmail.com',
+        '$2a$12$tpu0JGkNTesQz/1UifEQVe2PWAoFVsAfopYR1A5A.o9iR5dcbyqNa', 'Apple', 1);
