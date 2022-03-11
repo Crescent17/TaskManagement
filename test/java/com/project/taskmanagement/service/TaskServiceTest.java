2c9069d35a745c2302acfd7bf1332eec22e898c3
Crescent17 <maratos2001@mail.ru>
2022-03-11 15:38:19 +0300

    test: add authorization through annotation

--- test/java/com/project/taskmanagement/service/TaskServiceTest.java
+++ test/java/com/project/taskmanagement/service/TaskServiceTest.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,59 +18,42 @@ public class TaskServiceTest {
 
     @Autowired
     private TaskService taskService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignTask() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Saved";
-        String actual = taskService.assignTask("Adidas", 2L, new Task("Assigned task"));
+        String actual = taskService.assignTask("Nike", 1L, new Task("Assigned task"));
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignTaskForEmployeeFromAnotherCompany() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Employee with such id doesn't belong to this company!";
-        String actual = taskService.assignTask("Adidas", 1L, new Task("Another company"));
+        String actual = taskService.assignTask("Nike", 2L, new Task("Another company"));
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignTaskForEmployeeWithWrongId() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Employee with such id doesn't exist!";
-        String actual = taskService.assignTask("Adidas", 0L, new Task("Wrong ID"));
+        String actual = taskService.assignTask("Nike", 10L, new Task("Wrong ID"));
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignTaskWithoutPermission() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "You don't have permission to assign tasks to users who are not in you company!";
-        String actual = taskService.assignTask("Nike", 4L, new Task("Without permission"));
+        String actual = taskService.assignTask("Amazon", 2L, new Task("Without permission"));
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignTaskForWrongCompany() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         Assertions.assertThrows(IllegalStateException.class, () -> {
             taskService.assignTask("Qwer", 4L, new Task("Wrong company"));
         });
@@ -82,38 +62,29 @@ public class TaskServiceTest {
     @Test
     void assignTaskWithoutAuthentication() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            taskService.assignTask("Adidas", 2L, new Task("Non authenticated"));
+            taskService.assignTask("Nike", 2L, new Task("Non authenticated"));
         });
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void deleteTask() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Deleted!";
-        String actual = taskService.deleteTask("Adidas", 2L);
+        String actual = taskService.deleteTask("Nike", 1L);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void deleteNonExistingTask() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "No task with such id!";
-        String actual = taskService.deleteTask("Adidas", 10L);
+        String actual = taskService.deleteTask("Nike", 10L);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void deleteTaskForWrongCompany() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         Assertions.assertThrows(IllegalStateException.class, () -> {
             taskService.deleteTask("Eweqr", 5L);
         });
@@ -122,38 +93,29 @@ public class TaskServiceTest {
     @Test
     void deleteTaskWithoutAuthentication() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            taskService.deleteTask("Adidas", 2L);
+            taskService.deleteTask("Nike", 2L);
         });
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void updateTask() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Changed!";
-        String actual = taskService.updateTask("Adidas", 2L, new Task("Changed task"));
+        String actual = taskService.updateTask("Nike", 1L, new Task("Changed task"));
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void updateTaskWithWrongId() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "The task with such id is not found!";
-        String actual = taskService.updateTask("Adidas", 10L, new Task("Wrong id"));
+        String actual = taskService.updateTask("Nike", 10L, new Task("Wrong id"));
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void updateTaskForWrongCompany() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         Assertions.assertThrows(IllegalStateException.class, () -> {
             taskService.updateTask("Qwerq", 5L, new Task("Wrong company"));
         });
@@ -162,60 +124,45 @@ public class TaskServiceTest {
     @Test
     void updateTaskWithoutAuthentication() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            taskService.updateTask("Adidas", 2L, new Task("Non authenticated"));
+            taskService.updateTask("Nike", 2L, new Task("Non authenticated"));
         });
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignToOtherEmployee() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "The task was reassigned!";
-        String actual = taskService.assignToOtherEmployee("Adidas", 1L, 3L);
+        String actual = taskService.assignToOtherEmployee("Nike", 1L, 4L);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignToEmployeeFromAnotherCompany() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
-        String expected = "This user doesn't belong to Adidas company";
-        String actual = taskService.assignToOtherEmployee("Adidas", 1L, 1L);
+        String expected = "This user doesn't belong to Nike company";
+        String actual = taskService.assignToOtherEmployee("Nike", 1L, 2L);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignToEmployeeWithWrongId() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Wrong employee id";
-        String actual = taskService.assignToOtherEmployee("Adidas", 1L, 10L);
+        String actual = taskService.assignToOtherEmployee("Nike", 1L, 10L);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignToEmployeeWrongTaskId() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         String expected = "Wrong task id";
-        String actual = taskService.assignToOtherEmployee("Adidas", 10L, 3L);
+        String actual = taskService.assignToOtherEmployee("Nike", 10L, 1L);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void assignToEmployeeWithWrongCompanyName() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         Assertions.assertThrows(IllegalStateException.class, () -> {
             taskService.assignToOtherEmployee("wqrwrweq", 5L, 2L);
         });
@@ -224,36 +171,30 @@ public class TaskServiceTest {
     @Test
     void assignToEmployeeWithoutAuthentication() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            taskService.assignToOtherEmployee("Adidas", 2L, 5L);
+            taskService.assignToOtherEmployee("Nike", 2L, 5L);
         });
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void companyValidation() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         boolean expected = true;
-        boolean actual = taskService.companyValidation("Adidas");
+        boolean actual = taskService.companyValidation("Nike");
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    @WithMockUser(username = "nike@gmail.com", password = "123456")
     void companyValidationForWrongCompanyName() {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("adidas@gmail.com", "123456");
-        Authentication authentication = authenticationManager.authenticate(authReq);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
         boolean expected = false;
-        boolean actual = taskService.companyValidation("Nike");
+        boolean actual = taskService.companyValidation("Amazon");
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void companyValidationWithoutAuthentication() {
         Assertions.assertThrows(NullPointerException.class, () -> {
-            taskService.companyValidation("Adidas");
+            taskService.companyValidation("Nike");
         });
     }
 }
