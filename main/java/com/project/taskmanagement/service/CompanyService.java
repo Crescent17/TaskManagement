6e199222a103d8dd1f687b23dbfb22ecb441bf99
Crescent17 <maratos2001@mail.ru>
2022-03-16 11:47:00 +0300

    feat: change logic of services

--- main/java/com/project/taskmanagement/service/CompanyService.java
+++ main/java/com/project/taskmanagement/service/CompanyService.java
@@ -1,8 +1,8 @@
 package com.project.taskmanagement.service;
 
+import com.project.taskmanagement.exception.*;
 import com.project.taskmanagement.model.Company;
 import com.project.taskmanagement.model.Employee;
-import com.project.taskmanagement.model.Task;
 import com.project.taskmanagement.repository.CompanyRepository;
 import com.project.taskmanagement.repository.EmployeeRepository;
 import com.project.taskmanagement.repository.TaskRepository;
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class CompanyService implements UserDetailsService {
@@ -67,7 +65,7 @@ public class CompanyService implements UserDetailsService {
         if (!company.isEmpty()) {
             return company;
         } else {
-            throw new IllegalStateException("Wrong company name");
+            throw new WrongCompanyName();
         }
     }
 
@@ -76,5 +74,67 @@ public class CompanyService implements UserDetailsService {
         String username = authentication.getName();
         Company company = companyRepository.findByUsername(username).get(0);
         return employeeRepository.findByCompanyId(company.getCompanyId());
+    }
+
+    public String changeCompanyName(String previousName, String newName) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Company company = companyRepository.findByUsername(authentication.getName()).get(0);
+        if (company.getName().equalsIgnoreCase(previousName)) {
+            company.setName(newName);
+            return "Changed!";
+        }
+        throw new WrongCompanyName();
+    }
+
+    public String changeCompanyUsername(String previousUsername, String newUsername) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Company company = companyRepository.findByUsername(authentication.getName()).get(0);
+        if (company.getUsername().equalsIgnoreCase(previousUsername)) {
+            if (companyRepository.findByUsername(newUsername).isEmpty()) {
+                company.setUsername(newUsername);
+                return "Changed!";
+            }
+            throw new EntityExistsException();
+        }
+        throw new NoPermission();
+    }
+
+    public String changeCompanyPassword(String companyName, String previousPassword, String newPassword) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Company> companyOptional = companyRepository.findByUsername(authentication.getName());
+        if (!companyOptional.isEmpty()) {
+            Company company = companyOptional.get(0);
+            if (company.getName().equalsIgnoreCase(companyName)) {
+                if (myPasswordEncoder.getPasswordEncoder().matches(previousPassword, company.getPassword())) {
+                    company.setPassword(myPasswordEncoder.getPasswordEncoder().encode(newPassword));
+                    return "Changed!";
+                }
+                throw new PasswordsDontMatch();
+            }
+            throw new NoPermission();
+        }
+        throw new AuthorizationException();
+    }
+
+    public String deleteEmployee(String companyName, String employeeUsername) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Company> companyOptional = companyRepository.findByUsername(authentication.getName());
+        if (!companyOptional.isEmpty()) {
+            Company company = companyOptional.get(0);
+            if (company.getName().equalsIgnoreCase(companyName)) {
+                List<Employee> employeeOptional = employeeRepository.findByUsername(employeeUsername);
+                if (!employeeOptional.isEmpty()) {
+                    Employee employee = employeeOptional.get(0);
+                    if (employee.getCompanyName().equalsIgnoreCase(companyName)) {
+                        employeeRepository.delete(employee);
+                        return "Deleted!";
+                    }
+                    throw new NoPermission();
+                }
+                throw new WrongUsername();
+            }
+            throw new NoPermission();
+        }
+        throw new AuthorizationException();
     }
 }
--- main/java/com/project/taskmanagement/service/EmployeeService.java
+++ main/java/com/project/taskmanagement/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.project.taskmanagement.service;
 
+import com.project.taskmanagement.exception.*;
 import com.project.taskmanagement.model.Company;
 import com.project.taskmanagement.model.Employee;
 import com.project.taskmanagement.model.Task;
@@ -7,6 +8,8 @@ import com.project.taskmanagement.repository.CompanyRepository;
 import com.project.taskmanagement.repository.EmployeeRepository;
 import com.project.taskmanagement.util.MyPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -40,7 +43,7 @@ public class EmployeeService implements UserDetailsService {
 
     public String register(Employee employee) {
         if (companyRepository.findByNameIgnoreCase(employee.getCompanyName()).isEmpty()) {
-            throw new IllegalStateException("There is no such company registered!");
+            throw new WrongCompanyName();
         } else if (!employeeRepository.findByUsername(employee.getUsername()).isEmpty()) {
             throw new EntityExistsException("Employee with this login already exists!");
         }
@@ -58,7 +61,65 @@ public class EmployeeService implements UserDetailsService {
             employee.getTask().add(task);
             return "Assigned";
         } else {
-            throw new IllegalStateException("Wrong id");
+            throw new WrongEmployeeId();
         }
+    }
+
+    public String changeEmployeeName(String previousName, String newName) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Employee employee = employeeRepository.findByUsername(authentication.getName()).get(0);
+        if (employee.getName().equalsIgnoreCase(previousName)) {
+            employee.setName(newName);
+            return "Changed!";
+        }
+        throw new WrongEmployeeName();
+    }
+
+    public String changeEmployeeLastName(String lastName, String newLastName) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Employee employee = employeeRepository.findByUsername(authentication.getName()).get(0);
+        if (employee.getLastName().equalsIgnoreCase(lastName)) {
+            employee.setLastName(newLastName);
+            return "Changed!";
+        }
+        throw new WrongEmployeeLastName();
+    }
+
+    public String changeEmployeeUsername(String username, String newUsername) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Employee employee = employeeRepository.findByUsername(authentication.getName()).get(0);
+        if (employee.getUsername().equalsIgnoreCase(username)) {
+            if (employeeRepository.findByUsername(newUsername).isEmpty()) {
+                employee.setUsername(newUsername);
+                return "Changed!";
+            }
+            throw new EntityExistsException();
+        }
+        throw new WrongUsername();
+    }
+
+    public String changeEmployeePassword(String password, String newPassword) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Employee> employeeOptional = employeeRepository.findByUsername(authentication.getName());
+        if (!employeeOptional.isEmpty()) {
+            Employee employee = employeeOptional.get(0);
+            if (myPasswordEncoder.getPasswordEncoder().matches(password, employee.getPassword())) {
+                employee.setPassword(myPasswordEncoder.getPasswordEncoder().encode(newPassword));
+                return "Changed!";
+            }
+            throw new PasswordsDontMatch();
+        }
+        throw new AuthorizationException();
+    }
+
+    public String changeCompany(String companyName) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Employee employee = employeeRepository.findByUsername(authentication.getName()).get(0);
+        if (!companyRepository.findByNameIgnoreCase(companyName).isEmpty()) {
+            employee.setCompany(companyRepository.findByNameIgnoreCase(companyName).get(0));
+            employee.setCompanyName(companyRepository.findByNameIgnoreCase(companyName).get(0).getName());
+            return "Changed!";
+        }
+        throw new WrongCompanyName();
     }
 }
