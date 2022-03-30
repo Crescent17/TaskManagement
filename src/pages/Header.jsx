c245ca37dfb0674f7c40c0bea326eb5fe7be0cf2
Crescent17 <maratos2001@mail.ru>
2022-03-30 13:11:35 +0300

    refactor: refactor header component

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -14,19 +14,19 @@ const Header = () => {
                     paddingTop: "15px",
                     fontSize: "20px"
                 }}>
-                <Link href="/" underline={"none"} position={"absolute"} display={"flex"} marginLeft="1%"
-                      color={"white"}>Task Manager</Link>
-                {AuthenticationService.isEmployeeLoggedIn() &&
-                <Link href="/employee/cabinet" underline={"none"} position={"absolute"} display={"inline-block"}
-                      textAlign={"center"}
-                      marginLeft="49%" color={"white"}>Home</Link>}
-                {AuthenticationService.isCompanyLoggedIn() &&
-                <Link href="/company/cabinet" underline={"none"} position={"absolute"} display={"flex"}
-                      textAlign={"center"}
-                      marginLeft="49%" color={"white"}>Home</Link>}
-                {(AuthenticationService.isEmployeeLoggedIn() || AuthenticationService.isCompanyLoggedIn()) &&
-                <Link href="/" position={"absolute"} underline={"none"} display={"inline-block"} right="0"
-                      marginRight="2%" color={"white"} onClick={() => AuthenticationService.logout()}>Logout</Link>}
+            <Link href="/" underline={"none"} position={"absolute"} display={"flex"} marginLeft="1%"
+                  color={"white"}>Task Manager</Link>
+            {AuthenticationService.isEmployeeLoggedIn() &&
+            <Link href="/employee/cabinet" underline={"none"} position={"absolute"} display={"inline-block"}
+                  textAlign={"center"}
+                  marginLeft="49%" color={"white"}>Home</Link>}
+            {AuthenticationService.isCompanyLoggedIn() &&
+            <Link href="/company/cabinet" underline={"none"} position={"absolute"} display={"flex"}
+                  textAlign={"center"}
+                  marginLeft="49%" color={"white"}>Home</Link>}
+            {(AuthenticationService.isEmployeeLoggedIn() || AuthenticationService.isCompanyLoggedIn()) &&
+            <Link href="/" position={"absolute"} underline={"none"} display={"inline-block"} right="0"
+                  marginRight="2%" color={"white"} onClick={() => AuthenticationService.logout()}>Logout</Link>}
         </AppBar>
     );
 };
